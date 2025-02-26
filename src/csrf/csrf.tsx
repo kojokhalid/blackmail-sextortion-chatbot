7651e9b512043f3f684653e64f410ce633f5b5fd
Antoh SHadrack <129493093+kojokhalid@users.noreply.github.com>
2025-02-26 01:22:19 +0000

    Delete src directory

--- src/csrf/csrf.tsx
+++
@@ -1,8 +0,0 @@
-// csrf.js
-export async function getCsrfToken() {
-  const response = await fetch("/api/get-csrf-token/", {
-    credentials: "include",
-  });
-  const data = await response.json();
-  return data.csrfToken;
-}
